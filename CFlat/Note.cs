7751196564c6f2138f47b88d7fa8b2aa3c580a49
andrewLCS <ajlitcofsky@gmail.com>
2026-03-16 20:09:27 -0400

    defining Notes and parsing notes

--- CFlat/Note.cs
+++ CFlat/Note.cs
@@ -1,32 +1,66 @@
-﻿namespace CFlat;
+﻿using System.Security.AccessControl;
+
+namespace CFlat;
 
 public struct Note
 {
-    private char letter;
-    private byte value;
+    private enum Letter
+    {
+        A, B, C, D, E, F, G
+    }
+    private enum Accent
+    {
+        DoubleFlat = -2,
+        Flat = -1,
+        Natural = 0,
+        Sharp = 1,
+        DoubleSharp = 2
+    }
+    private Letter letter;
+    private Accent accent;
+    private int octave;
 
     public Note()
     {
-        letter = 'C';
-        value = 3; // "middle C"
+        letter = Letter.C;
+        accent = Accent.Natural;
+        octave = 3; // "middle C"
     }
 
-    public Note(char letter, byte value)
+    public Note(string note)
     {
-        if (!(letter == 'A' || letter == 'B' || letter == 'C'
-            || letter == 'D' || letter == 'E' || letter == 'F' || letter == 'G')
-            ||
-            (value < 0 && value > 8))
-        {
-            throw new ArgumentException("Invalid note entered. Notes are written as a letter [A-G] followed by a value [0-8]. (ex. D3)");
-        }
-
+        (Letter letter, Accent accent, int octave) = ParseNote(note);
         this.letter = letter;
-        this.value = value;
+        this.accent = accent;
+        this.octave = octave;
     }
 
     public override readonly string ToString()
     {
-        return letter + "" + value;
+        return letter.ToString() + AccentToString() + octave;
     }
+
+    private readonly string AccentToString() =>
+        accent switch 
+        {
+            Accent.DoubleFlat => "bb",
+            Accent.Flat => "b",
+            Accent.Natural => "",
+            Accent.Sharp => "#",
+            Accent.DoubleSharp => "##",
+            _ => throw new ArgumentException($"Invalid accent value {accent}")
+        };
+
+    private static (Letter, Accent, int) ParseNote(string note)
+    {
+        if (string.IsNullOrEmpty(note))
+            throw new ArgumentException("Note string cannot be null or empty.");
+
+        char letterChar = note[0];
+        if (!Enum.TryParse(letterChar.ToString(), true, out Letter letter)) 
+            throw new ArgumentException($"Invalid note letter '{letterChar}'. Must be A-G");
+
+        return (Letter.C, Accent.Natural, 3);
+    }
+
 }
